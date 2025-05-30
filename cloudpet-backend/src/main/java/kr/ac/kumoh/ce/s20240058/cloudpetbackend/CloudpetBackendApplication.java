db5b0ecc4afa3112925a6b92c42b428c9f16d2cc
kaye <jhan117@naver.com>
2025-05-30 18:25:00 +0900

    🎉 Initialize project

---
+++ cloudpet-backend/src/main/java/kr/ac/kumoh/ce/s20240058/cloudpetbackend/CloudpetBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20240058.cloudpetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudpetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudpetBackendApplication.class, args);
+    }
+
+}
