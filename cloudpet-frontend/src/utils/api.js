ef3d0900b6e6911b78f11ae3e85bb1c05cd096b2
kaye <jhan117@naver.com>
2025-06-20 02:50:46 +0900

    🐛 fix typo in environment variable name

--- cloudpet-frontend/src/utils/api.js
+++ cloudpet-frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:8080/api";
-const BASE_URL = process.env.REACT_APP_API_URLl;
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const requestAPI = async (url, options, errorMsg) => {
   try {
